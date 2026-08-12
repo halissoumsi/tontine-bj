1e8d7ba25980f0f91e88c4f04e1dc7c0c0004520
SEIDOU IBRAHIM Mouhamadou Halissou <fofana1491@gmail.com>
2026-08-12 21:40:37 +0100

    chore(clean): remove all manus artifacts, plugins and references

--- client/src/hooks/usePersistFn.ts
+++ client/src/hooks/usePersistFn.ts
@@ -1,20 +1,23 @@
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 
-type noop = (...args: any[]) => any;
+type AnyFunction = (...args: any[]) => any;
 
 /**
- * usePersistFn instead of useCallback to reduce cognitive load
+ * usePersistFn guarantees a persistent callback reference while always calling the latest fn.
  */
-export function usePersistFn<T extends noop>(fn: T) {
+export function usePersistFn<T extends AnyFunction>(fn: T): T {
   const fnRef = useRef<T>(fn);
-  fnRef.current = fn;
 
-  const persistFn = useRef<T>(null);
+  useLayoutEffect(() => {
+    fnRef.current = fn;
+  });
+
+  const persistFn = useRef<T | null>(null);
   if (!persistFn.current) {
-    persistFn.current = function (this: unknown, ...args) {
-      return fnRef.current!.apply(this, args);
+    persistFn.current = function (this: unknown, ...args: Parameters<T>): ReturnType<T> {
+      return fnRef.current.apply(this, args);
     } as T;
   }
 
-  return persistFn.current!;
+  return persistFn.current;
 }
